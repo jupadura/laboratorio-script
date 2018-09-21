b157780844010c73acc09ad5d7d64d0b87da03f0
Juan Pablo Duque Ramirez <juan.duque@globant.com>
2018-09-21 14:32:19 -0500

    Adds random param to prevent caching

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var baseUrl = 'https://raw.githubusercontent.com/jupadura/laboratorio-script/master/';
+var random = (Math.random() + Date()).replace(/[\W]/g, '');
 
-fetch(`${baseUrl}src/lab.js`)
+fetch(`${baseUrl}src/lab.js?${random}`)
     .then(response => response.text())
     .then(eval);
 
-fetch(`${baseUrl}src/lab.css`)
+fetch(`${baseUrl}src/lab.css?${random}`)
     .then(response => response.text())
     .then(content => {
         var style = document.createElement('style');
@@ -12,7 +13,7 @@ fetch(`${baseUrl}src/lab.css`)
         document.head.appendChild(style);
     });
 
-fetch(`${baseUrl}src/lab.html`)
+fetch(`${baseUrl}src/lab.html?${random}`)
     .then(response => response.text())
     .then(content => {
         document.body.innerHTML = content;
